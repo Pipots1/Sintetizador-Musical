cc2169dbb2e182a4e8774cd4f4ec841a97a242e4
Sandra <eidenphantom@gmail.com>
2025-05-19 18:24:56 +0200

    botones.cpp

--- Sintetizador musical/src/botones.cpp
+++ Sintetizador musical/src/botones.cpp
@@ -1,64 +1,28 @@
-#include "config.h"
-#include <Arduino.h>
+#include "botones.h"
+#include <iostream>
 
-// Clase para gestionar un botón con debounce
-class Boton {
-  private:
-    uint8_t pin;
-    unsigned long lastPressTime;
-    unsigned long debounceDelay;
-    bool lastState;
+Boton::Boton(uint8_t pin, unsigned long debounceDelay)
+  : pin(pin), debounceDelay(debounceDelay), lastPressTime(0), lastState(HIGH) {}
 
-  public:
-    Boton(uint8_t pin, unsigned long debounceDelay = 200)
-        : pin(pin), debounceDelay(debounceDelay), lastPressTime(0), lastState(HIGH) {}
-
-    void iniciar() {
-        pinMode(pin, INPUT_PULLUP);
-    }
-
-    // Retorna true només si el botó s'ha premut (amb debounce)
-    bool fuePresionado() {
-        bool estadoActual = digitalRead(pin);
-        unsigned long ahora = millis();
-
-        if (estadoActual == LOW && lastState == HIGH && (ahora - lastPressTime > debounceDelay)) {
-            lastPressTime = ahora;
-            lastState = estadoActual;
-            return true;
-        }
-
-        lastState = estadoActual;
-        return false;
-    }
-};
-
-// Variables globals del sistema
-int modo_onda = 0;
-int efecto = 0;
-int modo_auto = 0;
-
-// Crear instàncies dels 3 botons
-Boton boton1(PIN_BOTON1);
-Boton boton2(PIN_BOTON2);
-Boton boton3(PIN_BOTON3);
-
-void iniciarBotones() {
-    boton1.iniciar();
-    boton2.iniciar();
-    boton3.iniciar();
+void Boton::iniciar() {
+    pinMode(pin, INPUT_PULLUP);
 }
 
-void leerBotones() {
-    if (boton1.fuePresionado()) {
-        modo_onda = (modo_onda + 1) % 3;
+bool Boton::fuePresionado() {
+    bool estadoActual = digitalRead(pin);
+    unsigned long ahora = millis();
+
+    if (estadoActual == LOW && lastState == HIGH && (ahora - lastPressTime > debounceDelay)) {
+        lastPressTime = ahora;
+        lastState = estadoActual;
+        std::cout << "Boton en pin " << (int)pin << ": ENCENDIDO" << std::endl;
+        return true;
     }
 
-    if (boton2.fuePresionado()) {
-        efecto = !efecto;
+    if (estadoActual == HIGH && lastState == LOW) {
+        std::cout << "Boton en pin " << (int)pin << ": APAGADO" << std::endl;
     }
 
-    if (boton3.fuePresionado()) {
-        modo_auto = !modo_auto;
-    }
+    lastState = estadoActual;
+    return false;
 }
